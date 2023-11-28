0f172e9557ab98573d56c54fcf92d7e901a49b82
Your Name <you@example.com>
2023-11-28 21:34:26 +0700

    endpoit login

--- handler/user.go
+++ handler/user.go
@@ -42,5 +42,34 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	response := helper.ApiResponse("Account has been registered", http.StatusOK, "succes", formatter)
 
 	c.JSON(http.StatusOK, response)
+}
 
+func (h *userHandler) LoginUser(c *gin.Context) {
+	var input user.LoginUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.ApiResponse(" Login account Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggeduser, err := h.userService.LoginUser(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.ApiResponse(" Login account Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggeduser, "tokentokentoken")
+
+	response := helper.ApiResponse("Login Succes", http.StatusOK, "succes", formatter)
+
+	c.JSON(http.StatusOK, response)
 }
--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func main() {
 	api := r.Group("api/v1")
 
 	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/sessions", userHandler.LoginUser)
 
 	r.Run()
 }
